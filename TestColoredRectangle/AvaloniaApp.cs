9878363dc0f9eb87269f54c174854538975f5602
Саша Боков <alex.skaut2@yandex.ru>
2023-02-12 23:24:42 +0700

    New tests

---
+++ TestColoredRectangle/AvaloniaApp.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Threading;
+using Avalonia;
+using Avalonia.ReactiveUI;
+using Avalonia.Headless;
+using ColoredRectangle.Views;
+using ColoredRectangle;
+using System;
+
+namespace TestColoredRectangle;
+
+public static class AvaloniaApp {
+	// DI registrations
+	/* public static void RegisterDependencies() =>
+				Bootstrapper.Register(AvaloniaLocator.CurrentMutable, AvaloniaLocator.Current);*/
+
+	// stop app and cleanup
+	public static void Stop () {
+		var app = GetApp();
+		if (app is IDisposable disposable) {
+			Dispatcher.UIThread.Post(disposable.Dispose);
+		}
+
+		Dispatcher.UIThread.Post(() => app?.Shutdown());
+	}
+
+	public static MainWindow? GetMainWindow () => GetApp()?.MainWindow as MainWindow;
+
+	public static IClassicDesktopStyleApplicationLifetime? GetApp () =>
+			Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
+
+	public static AppBuilder BuildAvaloniaApp () =>
+			AppBuilder
+					.Configure<App>()
+					.UsePlatformDetect()
+					.UseReactiveUI()
+					.UseHeadless(); // Need a package Avalonia.Headless 10.18 from NuGet for this method
+}
---
+++ TestColoredRectangle/UnitTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Avalonia.VisualTree;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
+
+namespace TestColoredRectangle;
+
+public class UnitTests {
+
+	[Fact]
+	public async void TestMainWindowHeightAndWidth () {
+		var window = AvaloniaApp.GetMainWindow();
+
+		await Task.Delay(100);
+
+		var expectedWidth = 300;
+		var expectedHeight = 300;
+
+		Assert.Equal(expectedHeight, window?.Height);
+		Assert.Equal(expectedWidth, window?.Width);
+	}
+
+	[Fact]
+	public async void TestColorChanging () {
+		var app = AvaloniaApp.GetApp();
+		var window = AvaloniaApp.GetMainWindow();
+
+		await Task.Delay(100);
+
+		var buttons = window.GetVisualDescendants().OfType<Button>();
+
+		foreach (var button in buttons) {
+			var expectedColorName = button.Content as string;
+
+			button.Command.Execute(button.CommandParameter);
+
+			var rectangle = button.GetVisualDescendants().OfType<Rectangle>().First();
+
+			await Task.Delay(50);
+
+			var rectColor = (rectangle.Fill as SolidColorBrush)?.Color;
+
+			Assert.StrictEqual(Color.Parse(expectedColorName), rectColor);
+		}
+	}
+
+}
